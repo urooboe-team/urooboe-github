559495a70526cb00c07822771d2d001d76c6d8d5
tomoki-ikarashi <lb.s0172@gmail.com>
2020-02-11 09:05:29 +0000

    update-product-new.html

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,11 +1,19 @@
 class Admin::ProductsController < ApplicationController
   def new
+    @product = Product.new
+    @genre = Genre.new
+
   end
 
   def index
+    @products = Product.all
+    # @genre = Genre.all
   end
 
   def show
+     @product = Product.find(params[:id])
+    # @genre = Genre.find(params[:id])
+
   end
 
   def create
@@ -16,4 +24,8 @@ class Admin::ProductsController < ApplicationController
 
   def update
   end
+  private
+    def product_params
+        params.require(:product).permit(:product_name,:product_description,:genre_name,:unit_price,:product_image_id,:sale_status)
+    end
 end
--- db/migrate/20200208092804_delete_addless.rb
+++
@@ -1,5 +0,0 @@
-class DeleteAddless < ActiveRecord::Migration[5.2]
-  def change
-  	drop_table :addlesses
-  end
-end
