7a5d2d6738366f5285a42c45f1058d1a62db53c4
mitsu733 <tubbbbbu@yahoo.co.jp>
2020-02-12 06:16:53 +0000

    address edit update

--- db/migrate/20200210121810_add_customer_default.rb
+++
@@ -1,5 +0,0 @@
-class AddCustomerDefault < ActiveRecord::Migration[5.2]
-  def change
-  	change_column_default :customers, :member_status, true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -41,26 +41,6 @@ ActiveRecord::Schema.define(version: 2020_02_11_061044) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "customers", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "kana_last_name", null: false
-    t.string "kana_first_name", null: false
-    t.string "postcode", null: false
-    t.string "address", null: false
-    t.string "telephone", null: false
-    t.boolean "member_status", default: true, null: false
-    t.index ["email"], name: "index_customers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
-  end
-
   create_table "genres", force: :cascade do |t|
     t.string "genre_name"
     t.boolean "display"
@@ -79,13 +59,8 @@ ActiveRecord::Schema.define(version: 2020_02_11_061044) do
   end
 
   create_table "orders", force: :cascade do |t|
-<<<<<<< HEAD
-    t.string "user_id"
-    t.integer "order_status"
-=======
     t.string "customer_id"
-    t.integer "order_status", default: 0
->>>>>>> master
+    t.integer "order_status"
     t.integer "total_fee"
     t.integer "payment"
     t.integer "freight"
