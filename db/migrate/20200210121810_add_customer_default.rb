5706a9d7b7078fcf492dcdb50098dc6290dc22b8
tomoki-ikarashi <lb.s0172@gmail.com>
2020-02-12 10:03:36 +0000

    finish-admin-product

--- db/migrate/20200210121810_add_customer_default.rb
+++ db/migrate/20200210121810_add_customer_default.rb
@@ -1,5 +1,5 @@
 class AddCustomerDefault < ActiveRecord::Migration[5.2]
   def change
-  	change_column_default :customers, :member_status, true
+  	# change_column_default :customers, :member_status, true
   end
 end
