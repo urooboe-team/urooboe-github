785bd414b58ec129fb29ef48e4181038e205f59f
Shoriii <cerezos1206@gmail.com>
2020-02-13 09:42:26 +0000

    fix

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,5 +1,12 @@
 class OrderProduct < ApplicationRecord
   has_many :orders, dependent: :destroy
   belongs_to :product
+
+  enum producttion_status: {
+		着手不可: 0,
+		制作待ち: 1,
+		製作中: 2,
+		制作完了: 3,
+	}
 end
 
