f5bd629d456946b0241d033735f5f6a19845de44
mitsu733 <tubbbbbu@yahoo.co.jp>
2020-02-17 06:55:11 +0000

    cart_item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-	 enum sale_status: { 販売中: true, 売り切れ: false }
+	enum sale_status: { 販売中: true, 売り切れ: false }
     has_many :customers,  through: :cart_items
     has_many :cart_items, dependent: :destroy
 
@@ -7,7 +7,21 @@ class Product < ApplicationRecord
 	belongs_to :genre
 	attachment :product_image
 
-  def add_tax
-    self.unit_price = (unit_price * 1.1).round
-  end
+
+# def add_tax
+    # self.price = (cart_item.product.unit_price * * 1.1).round
+  # end
+
+
+
+
+   def add_tax(product_id)
+
+     tax_included = Product.find(product_id).unit_price.to_i*1.1
+     add_tax = tax_included.to_i
+   end
+
+  # def add_tax
+  #   self.add_tax = unit_price.to_i * 1.1.to_i
+  # end
 end
