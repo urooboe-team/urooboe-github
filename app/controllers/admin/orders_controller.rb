aa0107428daca8c63f21c8155e6bcb6484fe951a
Shoriii <cerezos1206@gmail.com>
2020-02-18 10:34:48 +0000

    update4

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,13 +1,20 @@
 class Admin::OrdersController < ApplicationController
   def top
-    # @order = Order.find(params[:id])
-    # @order = Order.find_by(id: params[:id])
+     # @order = Order.find(params[:id])
+     # @order = Order.find_by(id: params[:id])
     # # 変数@likes_countを定義
     # @likes_count = Like.where(order_id: @order.id).count
+    # @orders = Order.all
+    # @orders = Order.all
+    # @order = Order.where(created_at: 1.day.ago.all_day)
+
+    range = Date.today.beginning_of_day..Date.today.end_of_day
+    @order = Order.where(created_at: range)
   end
 
   def index
   	@orders = Order.all
+
   end
 
   def show
@@ -16,13 +23,20 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+    # @order = OrderProduct.new()
   	@order = Order.find(params[:id])
-  	order.update(order_params)
+
+    # @order_product = OrderProduct.find(@order.order_products_id)
+
+     if params[:order][:order_status] == "入金確認"
+       @order.order_products.update(production_status: '制作待ち')
+    end
+  	@order.update(order_params)
   	redirect_to admin_orders_path
   end
 
   private
   def order_params
-  	params.require(:order).permit(:order_status, :production_status)
+  	params.require(:order).permit(:order_status)
   end
 end
